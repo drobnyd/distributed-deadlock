2b4e8a35fd25d51e15b4d02fd5a8dfaec5687af0
Denis Drobny <xqv764@motorolasolutions.com>
2023-02-09 15:30:56 +0100

    Refactor setup to reuse the startup and start multiple servers.

--- service_a/test/service_a/server_test.exs
+++ service_a/test/service_a/server_test.exs
@@ -1,62 +1,61 @@
 defmodule ServiceA.ServerTest do
   use ExUnit.Case
 
-  test "working scenario" do
+  setup do
+    start_supervised!(AMQPLib.Producer)
+
     [service_a_node1, service_a_node2] = LocalCluster.start_nodes("service-a", 2)
 
     Support.DistTestHelper.setup_node(service_a_node1)
     Support.DistTestHelper.setup_node(service_a_node2)
 
-    Support.DistTestHelper.start_child(service_a_node1, {ServiceA.Server, id: 1})
     Support.DistTestHelper.start_child(service_a_node1, ServiceA.Consumer)
 
-    Support.DistTestHelper.start_child(service_a_node2, {ServiceA.Server, id: 2})
+    1..20
+    |> Enum.to_list()
+    |> Enum.each(fn id ->
+      Support.DistTestHelper.start_child(service_a_node1, {ServiceA.Server, id: id})
+    end)
+
     Support.DistTestHelper.start_child(service_a_node2, ServiceA.Consumer)
+
+    21..40
+    |> Enum.to_list()
+    |> Enum.each(fn id ->
+      Support.DistTestHelper.start_child(service_a_node2, {ServiceA.Server, id: id})
+    end)
 
     [service_b_node1, service_b_node2] = LocalCluster.start_nodes("service-b", 2)
 
     Support.DistTestHelper.setup_node(service_b_node1)
     Support.DistTestHelper.setup_node(service_b_node2)
 
-    Support.DistTestHelper.start_child(service_b_node1, {ServiceB.Server, id: 1})
     Support.DistTestHelper.start_child(service_b_node1, ServiceB.Consumer)
 
-    Support.DistTestHelper.start_child(service_b_node2, {ServiceB.Server, id: 2})
+    1..20
+    |> Enum.to_list()
+    |> Enum.each(fn id ->
+      Support.DistTestHelper.start_child(service_b_node1, {ServiceB.Server, id: id})
+    end)
+
     Support.DistTestHelper.start_child(service_b_node2, ServiceB.Consumer)
 
-    # Call
-    start_supervised!(AMQPLib.Producer)
+    21..40
+    |> Enum.to_list()
+    |> Enum.each(fn id ->
+      Support.DistTestHelper.start_child(service_b_node2, {ServiceB.Server, id: id})
+    end)
+
+    :ok
+  end
+
+  test "working scenario" do
     {:ok, reply, _meta} = AMQPLib.Producer.call("amq.direct", "service_a", "1")
     {reply_num, ""} = Integer.parse(reply)
     assert 1_000_001 == reply_num
   end
 
   test "deadlock scenario" do
-    [service_a_node1, service_a_node2] = LocalCluster.start_nodes("service-a", 2)
-
-    Support.DistTestHelper.setup_node(service_a_node1)
-    Support.DistTestHelper.setup_node(service_a_node2)
-
-    Support.DistTestHelper.start_child(service_a_node1, {ServiceA.Server, id: 42})
-    Support.DistTestHelper.start_child(service_a_node1, ServiceA.Consumer)
-
-    Support.DistTestHelper.start_child(service_a_node2, {ServiceA.Server, id: 2})
-    Support.DistTestHelper.start_child(service_a_node2, ServiceA.Consumer)
-
-    [service_b_node1, service_b_node2] = LocalCluster.start_nodes("service-b", 2)
-
-    Support.DistTestHelper.setup_node(service_b_node1)
-    Support.DistTestHelper.setup_node(service_b_node2)
-
-    Support.DistTestHelper.start_child(service_b_node1, {ServiceB.Server, id: 42})
-    Support.DistTestHelper.start_child(service_b_node1, ServiceB.Consumer)
-
-    Support.DistTestHelper.start_child(service_b_node2, {ServiceB.Server, id: 2})
-    Support.DistTestHelper.start_child(service_b_node2, ServiceB.Consumer)
-
-    # Call
-    start_supervised!(AMQPLib.Producer)
-
     try do
       AMQPLib.Producer.call("amq.direct", "service_a", "42")
       assert false
