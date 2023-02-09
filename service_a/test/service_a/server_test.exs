001484bb0f9d15d68977f75a377c4b1db75d2c0f
Denis Drobny <xqv764@motorolasolutions.com>
2023-02-09 15:20:21 +0100

    Add a deadlock scenario

--- service_a/test/service_a/server_test.exs
+++ service_a/test/service_a/server_test.exs
@@ -1,7 +1,7 @@
 defmodule ServiceA.ServerTest do
   use ExUnit.Case
 
-  test "start two instances of service a" do
+  test "working scenario" do
     [service_a_node1, service_a_node2] = LocalCluster.start_nodes("service-a", 2)
 
     Support.DistTestHelper.setup_node(service_a_node1)
@@ -33,5 +33,43 @@ defmodule ServiceA.ServerTest do
     {:ok, reply, _meta} = AMQPLib.Producer.call("amq.direct", "service_a", "1")
     {reply_num, ""} = Integer.parse(reply)
     assert 1_000_001 == reply_num
+  end
+
+  test "deadlock scenario" do
+    [service_a_node1, service_a_node2] = LocalCluster.start_nodes("service-a", 2)
+
+    Support.DistTestHelper.setup_node(service_a_node1)
+    Support.DistTestHelper.setup_node(service_a_node2)
+
+    Support.DistTestHelper.start_child(service_a_node1, {ServiceA.Server, id: 42})
+    Support.DistTestHelper.start_child(service_a_node1, ServiceA.Consumer)
+
+    # Support.DistTestHelper.start_child(service_a_node2, {ServiceA.Server, id: 2})
+
+    # Support.DistTestHelper.start_child(service_a_node2, AMQPLib.Producer)
+    # Support.DistTestHelper.start_child(service_a_node2, ServiceA.Consumer)
+
+    [service_b_node1, service_b_node2] = LocalCluster.start_nodes("service-b", 2)
+
+    Support.DistTestHelper.setup_node(service_b_node1)
+    Support.DistTestHelper.setup_node(service_b_node2)
+
+    Support.DistTestHelper.start_child(service_b_node1, {ServiceB.Server, id: 42})
+    Support.DistTestHelper.start_child(service_b_node1, ServiceB.Consumer)
+
+    # Support.DistTestHelper.start_child(service_b_node2, {ServiceB.Server, id: 2})
+
+    # Support.DistTestHelper.start_child(service_b_node2, AMQPLib.Producer)
+    # Support.DistTestHelper.start_child(service_b_node2, ServiceB.Consumer)
+
+    # Call
+    start_supervised!(AMQPLib.Producer)
+
+    try do
+      AMQPLib.Producer.call("amq.direct", "service_a", "42")
+      assert false
+    catch
+      :exit, {:timeout, _} -> assert true
+    end
   end
 end
