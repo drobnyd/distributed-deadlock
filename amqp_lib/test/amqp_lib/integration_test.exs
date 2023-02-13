ee530b0c30ffe5f3226a72dd4a643b840e6cbf8f
Denis Drobny <xqv764@motorolasolutions.com>
2023-02-13 17:55:17 +0100

    Move `docker-compose` into the individual projects

--- amqp_lib/test/amqp_lib/integration_test.exs
+++ amqp_lib/test/amqp_lib/integration_test.exs
@@ -1,6 +1,8 @@
 defmodule AMQPLib.IntegrationTest do
   @moduledoc """
   Tests the integration between `AMQP.Producer` and `AMQP.Consumer`.
+
+  Note that the suite requires running `docker-compose up -d amqp` from `amqp_lib/` directory.
   """
   use ExUnit.Case
 
@@ -11,30 +13,58 @@ defmodule AMQPLib.IntegrationTest do
   @exchange "amq.direct"
   @routing_key "routing_key"
 
-  test "greets the world" do
+  setup_all do
     username = Application.fetch_env!(:amqp_lib, :username)
     password = Application.fetch_env!(:amqp_lib, :password)
     host = Application.fetch_env!(:amqp_lib, :host)
     connection_params = [username: username, password: password, host: host]
 
-    start_supervised!({Producer, connection_params})
+    {:ok, %{connection_params: connection_params}}
+  end
 
-    request = "hello"
-    response = "world"
+  describe "greets the world" do
+    test "consumer uses auto-generated queue name", %{connection_params: connection_params} do
+      start_supervised!({Producer, connection_params})
 
-    start_supervised!(
-      {Consumer,
-       {
-         connection_params,
-         @exchange,
-         @routing_key,
-         "",
-         fn ^request, _meta ->
-           {:reply, response}
-         end
-       }}
-    )
+      request = "hello"
+      response = "world"
 
-    assert {:ok, ^response, _meta} = Producer.call(@exchange, @routing_key, request)
+      start_supervised!(
+        {Consumer,
+         {
+           connection_params,
+           @exchange,
+           @routing_key,
+           "",
+           fn ^request, _meta ->
+             {:reply, response}
+           end
+         }}
+      )
+
+      assert {:ok, ^response, _meta} = Producer.call(@exchange, @routing_key, request)
+    end
+
+    test "consumer uses static queue name", %{connection_params: connection_params} do
+      start_supervised!({Producer, connection_params})
+
+      request = "hello"
+      response = "world"
+
+      start_supervised!(
+        {Consumer,
+         {
+           connection_params,
+           @exchange,
+           @routing_key,
+           "queue-name",
+           fn ^request, _meta ->
+             {:reply, response}
+           end
+         }}
+      )
+
+      assert {:ok, ^response, _meta} = Producer.call(@exchange, @routing_key, request)
+    end
   end
 end
